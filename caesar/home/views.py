2bfc6bfc1f851738c233330a062b29f04fabaaeb
hurryxin <ruixinhurry@gmail.com>
2018-07-25 16:23:49 +0800

    finish home backend

--- caesar/home/views.py
+++ caesar/home/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    context = {}
+    return render(request, 'home/home.html', context)
