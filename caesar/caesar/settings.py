96fb3e8fe69a33b6bd879c39bfe0c2353327bd53
hurryxin <ruixinhurry@gmail.com>
2018-07-25 15:48:43 +0800

    create home app

--- caesar/caesar/settings.py
+++ caesar/caesar/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'caesar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'caesarFinal',
+        'USER': 'ruixin',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
